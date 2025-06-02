1af8e2104adae7cd97d73ffe8d0c131a215fe208
brx86 <brx86@qq.com>
2025-06-02 20:44:18 +0800

    only support windows

--- actions/run.go
+++ actions/run.go
@@ -2,13 +2,9 @@ package actions
 
 import (
 	"os/exec"
-	"runtime"
 )
 
 func OpenFileWindows(filePath string) {
-	if runtime.GOOS != "windows" {
-		println("此函数仅适用于 Windows 操作系统")
-	}
 	cmd := exec.Command("cmd", "/C", "start", "", filePath)
 	cmd.Run()
 }
