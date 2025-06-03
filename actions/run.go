bb644e3f3e772aabf9ac4dbba86a8d60795abaa4
brx86 <brx86@qq.com>
2025-06-03 16:29:17 +0800

    fix desc string in file chooser

--- actions/run.go
+++ actions/run.go
@@ -14,11 +14,11 @@ func OpenFileWindows(filePath string) func() {
 }
 
 func SelectFileWindows() string {
-	filePath, _ := dialog.File().Title("选择要打开的文件或程序").Filter("文件或程序", "*").Load()
+	filePath, _ := dialog.File().Title("选择要打开的文件或程序").Filter("文件或程序(*.*)", "*").Load()
 	return filePath
 }
 
 func SelectLnkWindows() string {
-	link, _ := dialog.File().Title("选择要打开的网页快捷方式").Filter("快捷方式", "lnk").Load()
+	link, _ := dialog.File().Title("选择要打开的网页快捷方式").Filter("快捷方式(*.lnk)", "lnk").Load()
 	return link
 }
